c1bc656354eb196bf4396a86e7b2a45a4391dce5
Kent Martin Åsvang <github@asvang.no>
2023-11-06 07:26:56 +0100

    added layout.svelte

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,2 +1,68 @@
-<h1> You are in posts </h1>
-<p> this is a post </p>
+<!-- Page Header-->
+<header class="masthead" style="background-image: url('../static/img/voxel-world-header.png')">
+
+    <div class="container position-relative px-4 px-lg-5">
+
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+
+            <div class="col-md-10 col-lg-8 col-xl-7">
+
+                <div class="post-heading">
+
+                    <h1>Fra kode til pixel</h1>
+
+                    <h2 class="subheading">Hode først ned i grafikkprogrammeringens verden</h2>
+
+                    <span class="meta">
+                        Postet av
+                        <a href="#!">Kent M. Åsvang</a>
+                        den 24 August, 2023
+                    </span>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</header>
+
+<!-- Post Content-->
+<article class="mb-4">
+
+    <div class="container px-4 px-lg-5">
+
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+
+            <div class="col-md-10 col-lg-8 col-xl-7">
+
+                <p> 
+                    Jeg kan ingenting om grafikkprogrammering. Jeg kan ingenting om å skrive blogg heller, men det er litt av poenget.
+                    Det har blitt sagt av prominente psykologer at å utsette seg for nye ting låser opp potensialet.
+                    Ikke bare metaforisk, men at man med å utsette seg for nye situasjoner faktisk aktiverer nye celler i hjernen og dermed skaper en varig endring av jeg'et. 
+                    Jeg kan bare ekstrapolere at dette gjelder for å lære seg nye ting også. Dog, det spiller ingen rolle. Hovedpoenget er å ha det gøy.
+                    <br>
+                    <strong style="color: red"> prøv å finn referanse til sovende celler i hjernen! Jordan Peterson snakker om dette</strong>
+                </p>
+
+                <h2 class="section-heading"> Hvor skal man begynne </h2>
+
+                <p>
+
+                </p>
+
+                <!-- <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p> -->
+                <!-- <h2 class="section-heading">The Final Frontier</h2> -->
+                <!-- <blockquote class="blockquote">The dreams of yesterday are the hopes of today and the reality of tomorrow. Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next ten.</blockquote> -->
+                <!-- <a href="#!"><img class="img-fluid" src="assets/img/post-sample-image.jpg" alt="..." /></a> -->
+                <!-- <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span> -->
+
+            </div>
+
+        </div>
+
+    </div>
+
+</article>
